6d60ea4f36a8a1bf90d993fe710607811d489e8c
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2020-03-10 19:42:49 +0100

    poc buycrypto

--- src/screens/Partners/index.js
+++ src/screens/Partners/index.js
@@ -1,57 +1,92 @@
 // @flow
 
-import React, { Component } from "react";
-import { Trans, translate } from "react-i18next";
-import { View, StyleSheet } from "react-native";
-// $FlowFixMe
-import { SafeAreaView, ScrollView } from "react-navigation";
-import partners from "@ledgerhq/live-common/lib/partners/reactNative";
-import type { NavigationScreenProp } from "react-navigation";
-import type { T } from "../../types/common";
-import PartnerCard from "../../components/PartnerCard";
+/**
+ * MOBILE problems
+ * - all the links need to be target _blank because right now it opens IN the iframe from a mobile webview. so it's really bad. it should open in navigator (safari / chrome)
+ * - the email field doesn't prevent the auto case of the keyboard.
+ * - for same reason as desktop. I can't test the "step events" for us to implement a special effect to trigger on device so i can't go farther in this investigation.
+ */
+
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-navigation";
+import { WebView } from "react-native-webview";
+import querystring from "querystring";
+import { useSelector } from "react-redux";
+import type { Account } from "@ledgerhq/live-common/lib/types";
+import { accountsSelector } from "../../reducers/accounts";
+import Button from "../../components/Button";
 import LText from "../../components/LText";
+
 import colors from "../../colors";
 import extraStatusBarPadding from "../../logic/extraStatusBarPadding";
-import TrackScreen from "../../analytics/TrackScreen";
 
 const forceInset = { bottom: "always" };
 
-type Props = {
-  navigation: NavigationScreenProp<*>,
-  t: T,
+const Main = ({ account, sandbox }: { account: Account, sandbox: boolean }) => {
+  const primaryColor = colors.live;
+  const fontColor = colors.darkBlue;
+  const url = sandbox
+    ? "https://trade-ui.sandbox.coinify.com/widget"
+    : "https://trade-ui.coinify.com/widget";
+  const partnerId = sandbox ? 104 : 119;
+
+  return (
+    <>
+      <WebView
+        source={{
+          uri:
+            url +
+            "?" +
+            querystring.stringify({
+              fontColor,
+              primaryColor,
+              partnerId,
+              cryptoCurrencies: account.currency.ticker,
+              address: account.freshAddress,
+            }),
+        }}
+      />
+    </>
+  );
 };
 
-class ExchangeScreen extends Component<Props> {
-  static navigationOptions = {
-    header: null,
-  };
+const Root = () => {
+  const accounts = useSelector(accountsSelector);
+  // ^FIXME in reality we would have to filter only supported accounts
+  const [account] = useState(accounts[0]);
+  const [sandbox] = useState(true);
+  // ^FIXME in reality we would have no sandbox mode here (but maybe a env var)
+  const [main, setMain] = useState(false);
 
-  render() {
-    const { t } = this.props;
-
-    return (
-      <SafeAreaView
-        style={[styles.root, { paddingTop: extraStatusBarPadding }]}
-        forceInset={forceInset}
-      >
-        <TrackScreen category="Partners" />
-        <ScrollView style={styles.scrollView}>
-          <View style={styles.body}>
-            <LText secondary style={styles.title} bold>
-              <Trans i18nKey="partners.title" />
-            </LText>
-            <LText secondary style={styles.description} numberOfLines={2}>
-              <Trans i18nKey="partners.subtitle" />
-            </LText>
-            {partners.map(card => (
-              <PartnerCard icon={card.Logo} key={card.id} t={t} card={card} />
-            ))}
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    );
+  if (main && account) {
+    return <Main account={account} sandbox={sandbox} />;
   }
-}
+
+  return (
+    <View style={{ flex: 1, padding: 20, justifyContent: "center" }}>
+      <LText style={{ marginBottom: 10 }}>
+        using: {(account && account.name) || "no account"}
+      </LText>
+      <Button type="primary" onPress={() => setMain(true)} title="Continue" />
+    </View>
+  );
+};
+
+const ExchangeScreen = () => {
+  return (
+    <SafeAreaView
+      style={[styles.root, { paddingTop: extraStatusBarPadding }]}
+      forceInset={forceInset}
+    >
+      <Root />
+    </SafeAreaView>
+  );
+};
+
+ExchangeScreen.navigationOptions = {
+  header: null,
+};
 
 const styles = StyleSheet.create({
   root: {
@@ -82,4 +117,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default translate()(ExchangeScreen);
+export default ExchangeScreen;
